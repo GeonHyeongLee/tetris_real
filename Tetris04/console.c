002bc0d25137f9cbd2385f5e0b271a14f53205cd
YHJ <139127025+HyeongJinYu@users.noreply.github.com>
2025-05-19 12:13:26 +0900

    Update console.c

--- Tetris04/console.c
+++ Tetris04/console.c
@@ -78,3 +78,41 @@ void console_display_info(const windows_console_t* console) {
     printf("[%d] Colors (fore,back) = (%s,%s)\r\n", ++dummy_count, get_color_string(console->fore_color), get_color_string(console->back_color));
     printf("[%d] Cursor (x,y,hide) = %d,%d,%d)\r\n", ++dummy_count, console->cursor_info.x, console->cursor_info.y, console->cursor_info.is_hide);
 }
+
+void console_init(windows_console_t* console) {
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    console_clear(console);
+
+    // STD_OUTPUT_HANDLE을 통해 콘솔 핸들 가져오기
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    // 콘솔 화면 버퍼 정보 가져오기
+    if (GetConsoleScreenBufferInfo(hConsole, &csbi)) {
+        // 현재 콘솔 창의 크기 정보 얻어오기
+        console->cols = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+        console->rows = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+
+        // 현재 콘솔 창의 fore, back 색상 정보 알아오기
+        WORD attributes = csbi.wAttributes;
+        console->fore_color = attributes & 0x0F;
+        console->back_color = (attributes >> 4) & 0x0F;
+
+        // 커서 정보 알아내기
+        console->cursor_info.x = csbi.dwCursorPosition.X; // 커서의 X 좌표
+        console->cursor_info.y = csbi.dwCursorPosition.Y; // 커서의 Y 좌표
+
+        console->cursor_info.is_hide = get_cursor_visibility();
+        //set_cursor_visibility(console->cursor_info.is_hide);
+
+        //console_resize(const windows_console_t * console, uint32_t cols, uint32_t rows) {
+        console_resize(&console, RESIZE_COLS, RESIZE_ROWS);
+    }
+    else {
+        printf("Unable to get console window information.\n");
+
+        printf("Error: Unable to get console cursor position.\n");
+        console->cursor_info.x = -1;
+        console->cursor_info.y = -1;
+    }
+    //printf("cosole (cols,rows)= %d,%d", console->cols, console->rows);
+}
